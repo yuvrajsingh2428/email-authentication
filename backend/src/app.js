e657856512242ced0bc6f4b654e8d936ee157c7f
yuvrajsingh2428 <offcyuvi2428@gmail.com>
2024-09-24 19:20:11 +0530

    vercel cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,16 @@ const express = require("express")
 const ApiError = require("./utils/ApiError")
 const httpStatus = require("http-status")
 const ErrorHandling = require("./middleware/ErrorHandling")
-const app = express()
 const cors = require('cors')
+const app = express()
 
+const allowedOrigins = ['http://localhost:5173','https://email-authentication-mgfm.vercel.app/login'];
 
-app.use(cors())
+app.use(cors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // If your frontend is sending cookies or authentication headers
+  }));
 app.use(express.json({limit: '16mb'}))
 app.use(express.urlencoded({extended: false}))
 
